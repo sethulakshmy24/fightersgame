b17ef246744960aced3038a817c28782df05d370
sethulakshmy24 <sethu29286@gmail.com>
2024-06-14 22:13:59 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [data, setData] = useState("");
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  })
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. = {data}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
